67ed2862b476794e6c35d067ad337a480c031cdb
pragya16 <pragya.g16@gmail.com>
2017-06-08 17:14:20 +0530

    Create program3.py

---
+++ program3.py
@@ -0,0 +1,16 @@
+# Question 3:Write a Python program to append text to a file and display the text
+
+
+def file_read(fname):  
+        from itertools import islice  
+        with open(fname, "w") as t1:  
+                t1.write("Python Exercises is good for learning python\n")  
+                t1.write("Java Exercises is good for increase knowldge")  
+        txt = open(fname)  
+        print(txt.read())  
+file_read('test.txt')  
+
+'''
+output:
+
+'''
