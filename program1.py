c81e37dc0ceb360cb4e91dc649faec55149f5908
pragya16 <pragya.g16@gmail.com>
2017-06-08 12:48:22 +0530

    Create program1.py

--- program1.py
+++ program1.py
@@ -1,10 +1,10 @@
-# Question 1: Write a Python program to read an entire text file and print its contents
+ # Question 1: Write a Python program to read an entire text file and print its contents
 
 
 # Open a file
 fo1 = open("zensar.txt", "w")
 fo1.write( "Python is a great language.\Now a days i am learning it.\it is intresting scripting language.\nYeah its great!!\n");
-str= fo1.read()
+str = fo1.read("zensar.txt")
 print "Read String is : ", str
 print "Name of the file: ", fo1.name
 print "Opening mode : ", fo1.mode
