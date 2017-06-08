5660d0e41764b3dfb247605cf02d940920dce7b2
pragya16 <pragya.g16@gmail.com>
2017-06-08 12:43:16 +0530

    Create program1.py

--- program1.py
+++ program1.py
@@ -2,9 +2,10 @@
 
 
 # Open a file
-fo1 = open("zensar.txt", "wb")
+fo1 = open("zensar.txt", "w")
 fo1.write( "Python is a great language.\Now a days i am learning it.\it is intresting scripting language.\nYeah its great!!\n");
-print fo1.read()
+str= fo1.read()
+print "Read String is : ", str
 print "Name of the file: ", fo1.name
 print "Opening mode : ", fo1.mode
 print "Softspace flag : ", fo1.softspace
