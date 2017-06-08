78d6bee0715dc187e094bf1b982f8908fe9e7c83
pragya16 <pragya.g16@gmail.com>
2017-06-08 12:49:27 +0530

    Create program1.py

--- program1.py
+++ program1.py
@@ -2,9 +2,9 @@
 
 
 # Open a file
-fo1 = open("zensar.txt", "w")
+fo1 = open("zensar.txt", "w+")
 fo1.write( "Python is a great language.\Now a days i am learning it.\it is intresting scripting language.\nYeah its great!!\n");
-str = fo1.read("zensar.txt")
+str = fo1.read()
 print "Read String is : ", str
 print "Name of the file: ", fo1.name
 print "Opening mode : ", fo1.mode
