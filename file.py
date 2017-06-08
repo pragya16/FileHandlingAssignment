f5c69de03550d1c98a1e750bd9a011cf4256c77d
pragya16 <pragya.g16@gmail.com>
2017-06-08 15:09:46 +0530

    Create file.py

--- file.py
+++ file.py
@@ -1,4 +1,4 @@
-file_path = 'C:\\temp\\sample.txt'
+file_path = 'D:\\sample.txt'
 file = open(file_path,"w")
 file.write('This is a writing text file demo.')
 file.write('you can add end-of-line character\n So this is a new line\n')
