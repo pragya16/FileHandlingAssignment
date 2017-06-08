147e28889c83fd902a4a1321b7f5dfd9e46ae8f1
pragya16 <pragya.g16@gmail.com>
2017-06-08 16:22:07 +0530

    Delete file.py

--- file.py
+++
@@ -1,9 +0,0 @@
-file_path = 'D:\\sample.txt'
-file = open(file_path,"w")
-file.write('This is a writing text file demo.')
-file.write('you can add end-of-line character\n So this is a new line\n')
-file.write(""" Another way to make a new line
-               in Python by using triple quotes""")
- 
-file.close()
-
