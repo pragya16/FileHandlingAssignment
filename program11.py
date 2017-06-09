3c17d5c18184ca175a6dc71438bd7d5b7ad0ea89
pragya16 <pragya.g16@gmail.com>
2017-06-09 14:53:48 +0530

    Create program11.py

--- program11.py
+++ program11.py
@@ -1,8 +1,15 @@
 # Question 11 :Write a Python program to write a list to a file
 
-thefile=open("text.txt","w+")
-for item in thelist:
-  print>>thefile, item
+
+fruit = ['apple', 'mango', 'banana', 'watermelon', 'papaya', 'lichi']  
+with open('text.txt', "w") as file:  
+        for f in fruit:  
+                file.write("%s\n" % f)  
+  
+content = open('text.txt')  
+print(content.read())  
+
+
 
 '''
 output:
