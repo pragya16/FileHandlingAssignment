fa5aca23f52d23c0de015c41d4ff0c8a17fb6c5d
pragya16 <pragya.g16@gmail.com>
2017-06-09 14:27:32 +0530

    Create program10.py

--- program10.py
+++ program10.py
@@ -1,4 +1,5 @@
 
+
 # Question 10 : Write a Python program to get the file size of a plain file
 
 import os
@@ -7,4 +8,6 @@ print str.st_size
 
 '''
 output:
+[root@demo FileHandlingAssignment]# python program10.py
+938
 '''
