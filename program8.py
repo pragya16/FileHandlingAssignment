5df24262d038db61b0eb24a68601ebf638175484
pragya16 <pragya.g16@gmail.com>
2017-06-09 12:44:20 +0530

    Create program8.py

--- program8.py
+++ program8.py
@@ -1,9 +1,8 @@
 # Question 8:Write a python program to find the longest words
 
 
-def Word():
-    text= "Python is a widely used high-level, general-purpose, interpreted, dynamic programming language"
-    long = ''
+text= "Python is a widely used high-level, general-purpose, interpreted, dynamic programming language"
+long = ''
 for word in text.split():
     if len(word) > long:
        long = len(word)
