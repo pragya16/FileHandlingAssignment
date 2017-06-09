921b8c4bf57d37789a1d3802b50dfb9cc65d5ecd
pragya16 <pragya.g16@gmail.com>
2017-06-09 15:52:49 +0530

    Create program14.py

--- program14.py
+++ program14.py
@@ -1,3 +1,4 @@
+
 # Question 14 : Write a Python program to read a random line from a file
 
 import random
@@ -7,4 +8,7 @@ print random.choice(str)
 
 '''
 output:
+output:
+[root@demo FileHandlingAssignment]# python program14.py
+apple
 '''
