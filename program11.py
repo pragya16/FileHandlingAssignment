445f174d1a60b5721b708612d2700714c62cf3b4
pragya16 <pragya.g16@gmail.com>
2017-06-09 14:39:55 +0530

    Create program11.py

---
+++ program11.py
@@ -0,0 +1,11 @@
+# Question 11 :Write a Python program to write a list to a file
+
+file=open("text.txt","w+")
+for item in file:
+  file.write("%s\n" % item)
+print >> file ,item
+
+'''
+output:
+
+'''
