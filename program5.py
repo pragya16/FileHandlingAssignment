fc6ccabf64cea3867e9b2750b4514253b1b8ab18
pragya16 <pragya.g16@gmail.com>
2017-06-09 10:46:09 +0530

    Create program5.py

---
+++ program5.py
@@ -0,0 +1,19 @@
+# Question 5:Write a Python program to read a file line by line and store it into a list
+
+def file_read(fname):
+        
+	with open(fname) as f2:
+                
+		#list is the list that contains the read lines.     
+                
+		list = f2.readlines()
+                
+		print(list)
+
+
+file_read('test.txt')
+
+'''
+output:
+
+''''
