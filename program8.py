e271ba7858a13db841a6d363d1a134bbbebfec94
pragya16 <pragya.g16@gmail.com>
2017-06-09 12:26:13 +0530

    Create program8.py

---
+++ program8.py
@@ -0,0 +1,17 @@
+# Question 8:Write a python program to find the longest words
+
+
+def Word():
+    file=  open('text.txt','r')
+    long=''
+    for line in file:
+    if len(line)>len(long):
+        long=line
+    return long
+print word()
+
+'''
+output:
+
+'''
+
