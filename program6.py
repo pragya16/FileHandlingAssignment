ed8e2f315b0c74076b52b664cf3555453ccf124a
pragya16 <pragya.g16@gmail.com>
2017-06-09 11:40:52 +0530

    Update program6.py

--- program6.py
+++ program6.py
@@ -1,6 +1,6 @@
 # Question 6 :Write a Python program to read a file line by line store it into a variable
 
-with open ("test.txt", "r") as file:
+with open ("test1.txt", "r") as file:
     str=file.readlines()
     print(str)
     
