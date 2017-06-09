bb09cfed8cf117ff7369c8acd4c854bcb7d24c12
pragya16 <pragya.g16@gmail.com>
2017-06-09 15:02:58 +0530

    Create program12.py

--- program12.py
+++ program12.py
@@ -1,8 +1,11 @@
+
 # Question 12 : Write a Python program to copy the contents of a file to another file
 
 from shutil import copyfile
 
 copyfile('text.txt', 'abc.txt')
+seek(0,0)
+print read('abc.txt')
 
 '''
 output:
