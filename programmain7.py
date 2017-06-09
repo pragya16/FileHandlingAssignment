3e56914da12033b74758b571c3e245ff40e6c56a
pragya16 <pragya.g16@gmail.com>
2017-06-09 12:15:54 +0530

    Create programmain7.py

---
+++ programmain7.py
@@ -0,0 +1,11 @@
+# Question 7 : Write a Python program to read a file line by line store it into an array
+f_array = []
+with open(''test.txt'',"r") as file:
+    for lines in file:
+        f_array.append(lines)
+    print(f_array)
+
+'''
+output:
+
+'''
